5f7d9c1998231024c80496ed6c99c077e699b671
6a6aQth <qylumanga@gmail.com>
2025-08-08 16:04:50 +0200

    Initial Second Commit

--- next/components/logo.tsx
+++ next/components/logo.tsx
@@ -11,17 +11,17 @@ export const Logo = ({ image, locale }: { image?: Image, locale?: string }) => {
     return (
       <Link
         href={`/${locale || 'en'}`}
-        className="font-normal flex space-x-2 items-center text-sm mr-4  text-black   relative z-20"
+        className="font-normal flex space-x-1 items-center text-sm mr-4  text-black   relative z-20"
       >
         <BlurImage
           src={strapiImage(image?.url)}
           alt={image.alternativeText}
           width={200}
           height={200}
-          className="h-10 w-10 rounded-xl mr-2"
+          className="h-16 w-16 rounded-xl mr-1"
         />
 
-        <span className="text-white font-bold">LaunchPad</span>
+        <span className="text-white font-bold text-lg">Aurum</span>
       </Link>
     );
   }
